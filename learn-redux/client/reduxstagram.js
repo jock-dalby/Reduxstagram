0081d07d9468f2da8922ed8886893a15c58eade3
Apprentice <apprentice@StMac0-6.local>
2016-10-04 16:12:50 +1300

    page rendering

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -14,13 +14,16 @@ import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import { Provider } from 'react-redux';
 import store, { history } from './store';
 
+// Router componenent needs a history object, which here is browserHistory which allows you to do push state without having to reload the page.
 
 const router = (
   <Provider store={store}>
     <Router history={history}>
       <Route path="/" component={App}>
+        // If the url matches "/" or a further extension of, grab the Main component.
         <IndexRoute component={PhotoGrid}></IndexRoute>
         <Route path="/view/:postId" component={Single}></Route>
+        // Then depending on the URL structure, either pass 'Main', 'PhotoGrid' or 'Single'.
       </Route>
     </Router>
   </Provider>
